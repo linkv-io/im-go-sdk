172508d22d2ffa6fd80d50ad831c9e87995e44d5
lijian@joyme.sg <123qweQWE!@#>
2021-04-26 19:23:49 +0800

    update

--- example/main.go
+++ example/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	appKey := "LM6000141252986692189573"
-	appSecret := "a436a3757c8b5da179d72dd37d24f81c"
+	appKey := "xxxxx"
+	appSecret := "xxxx"
 	im := imsdk.NewIM(appKey, appSecret)
 	thirdUID := "test-go-tob"
 	aID := "test"
 	// 进行帐号绑定
 	imToken, imOpenID, err := im.GetTokenByThirdUID(thirdUID, aID, "test-go",
-		imsdk.SexUnknown, "http://meet.linkv.sg/app/rank-list/static/img/defaultavatar.cd935fdb.png",
+		imsdk.SexUnknown, "http://xxxxx/app/rank-list/static/img/defaultavatar.cd935fdb.png",
 		"", "", "")
 	if err != nil {
 		panic("im.GetTokenByThirdUID(" + err.Error() + ")")
